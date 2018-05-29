ac37a3e08636edf4164afc2cb3d4cffe93278d03
Shuvo <zshuvo26@gmail.com>
2018-05-29 23:38:56 +0600

    first commit

---
+++ src/main/java/cloud/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package cloud.config.audit;
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('cloudApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
